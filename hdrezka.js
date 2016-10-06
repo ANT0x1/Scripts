8f82fe330e9f48d188f1da348e98938f9608c0b7
Tony <ANT0x1@users.noreply.github.com>
2016-10-06 23:53:49 +0300

    Create hdrezka.js

---
+++ hdrezka.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         HDRezka Ads Disabler
+// @namespace    ANT0x1
+// @version      1.0
+// @description  Disables all ads on this site.
+// @author       ANT0x1
+// @match        http://*.cdnapponline.com/video/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    adv_enabled = false;
+    console.log("Ads disabled.");
+})();
