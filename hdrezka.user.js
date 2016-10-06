37366e4a392a601dc17822b8dd6a58e926625c9b
Tony <ANT0x1@users.noreply.github.com>
2016-10-07 00:07:33 +0300

    Update hdrezka.user.js

--- hdrezka.user.js
+++ hdrezka.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         HDRezka Ads Disabler
 // @namespace    ANT0x1
-// @version      1.0
-// @description  Disables all ads on this site.
+// @version      1.1
+// @description  Disables all ads on hdrezka.me site.
 // @author       ANT0x1
 // @match        http://*.cdnapponline.com/video/*
 // @updateURL    https://openuserjs.org/meta/ANT0x1/HDRezka_Ads_Disabler.meta.js
 // @downloadURL  https://github.com/ANT0x1/Scripts/raw/master/hdrezka.user.js
 // @grant        none
-// @encoding     utf-8
 // @copyright 2016, ANT0x1
 // ==/UserScript==
 
