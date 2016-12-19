968be665be3d10cf6f3a24a83328993a522fa2cc
Tony <ANT0x1@users.noreply.github.com>
2016-12-19 15:57:28 +0200

    Update hdrezka.user.js

--- hdrezka.user.js
+++ hdrezka.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
-// @name         HDRezka Player Ads Disabler
-// @namespace    ANT0x1
-// @version      1.2.2
-// @date         2016-10-07
-// @description  Disables all ads in player on site HDRezka.
-// @author       ANT0x1
-// @include      http://*.cdnapponline.com/*
-// @updateURL    https://openuserjs.org/meta/ANT0x1/HDRezka_Player_Ads_Disabler.meta.js
-// @downloadURL  https://openuserjs.org/install/ANT0x1/HDRezka_Player_Ads_Disabler.user.js
-// @icon         http://hdrezka.me/templates/hdrezka/images/favicon.ico
-// @run-at       document-end
-// @homepage     https://openuserjs.org/scripts/ANT0x1/
-// @grant        none
-// @copyright 2016, ANT0x1
+// @name            HDRezka Player Ads Disabler
+// @name:en         HDRezka Player Ads Disabler
+// @namespace       ANT0x1
+// @version         1.2.3
+// @date            2016-12-19
+// @description     Отключает рекламу в плеере на HDRezka.
+// @description:en  Disables all ads in player on site HDRezka.
+// @author          ANT0x1
+// @include         http://*.cdnapponline.com/*
+// @updateURL       https://openuserjs.org/meta/ANT0x1/HDRezka_Player_Ads_Disabler.meta.js
+// @downloadURL     https://openuserjs.org/install/ANT0x1/HDRezka_Player_Ads_Disabler.user.js
+// @icon            http://hdrezka.me/templates/hdrezka/images/favicon.ico
+// @run-at          document-end
+// @homepage        https://openuserjs.org/scripts/ANT0x1/
+// @grant           none
+// @copyright       2016, ANT0x1
 // ==/UserScript==
 
 (function() {
