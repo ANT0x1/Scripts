25547b4e4a4035572288f50d6b04c35374c28358
Tony <ANT0x1@users.noreply.github.com>
2017-01-08 14:32:55 +0200

    Create job-mo.user.js

---
+++ job-mo.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name:ru         Job-MO: Автопоказ контактов
+// @name            Job-MO: Autodisplay contacts
+// @namespace       ANT0x1
+// @version         1.0
+// @date            2017-01-08
+// @description:ru  Автоматически отображает контакты при переходе на страницу вакансии.
+// @description     Auto display contacts at vacancy page.
+// @author          ANT0x1
+// @match           http://www.job-mo.ru/vac*.html
+// @icon            http://www.job-mo.ru/favicon.ico
+// @run-at          document-end
+// @homepage        https://openuserjs.org/scripts/ANT0x1/
+// @updateURL       https://openuserjs.org/meta/ANT0x1/Job-MO_Autodisplay_contacts.meta.js
+// @copyright       2017, ANT0x1
+// @grant           none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    cp();
+    cm();
+})();
