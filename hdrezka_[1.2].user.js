6e4655b01905704572b652e6b4e092708b4c10db
Tony <ANT0x1@users.noreply.github.com>
2017-04-01 15:46:48 +0300

    Update and rename hdrezka.user.js to hdrezka_[1.2].user.js

--- hdrezka_[1.2].user.js
+++ hdrezka_[1.2].user.js
@@ -2,13 +2,15 @@
 // @name            HDRezka Player Ads Disabler [1/2]
 // @name:en         HDRezka Player Ads Disabler [1/2]
 // @namespace       ANT0x1
-// @version         1.2.4
+// @version         1.3.0
 // @date            2017-04-01
 // @description     Отключает рекламу в плеере на HDRezka.
 // @description:en  Disables all ads in player on site HDRezka.
 // @author          ANT0x1
 // @match           http://*.cdnapponline.com/*/iframe*
 // @icon            http://hdrezka.me/templates/hdrezka/images/favicon.ico
+// @updateURL       https://openuserjs.org/meta/ANT0x1/HDRezka_Player_Ads_Disabler_[12].meta.js
+// @downloadURL     https://openuserjs.org/install/ANT0x1/HDRezka_Player_Ads_Disabler_[12].user.js
 // @run-at          document-end
 // @homepage        https://openuserjs.org/scripts/ANT0x1/
 // @grant           none
