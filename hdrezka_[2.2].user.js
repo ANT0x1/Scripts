e3ad73b277506baeff0cfedf3193f94790595c9b
Tony <ANT0x1@users.noreply.github.com>
2017-04-01 17:37:01 +0300

    Update hdrezka_[2.2].user.js

--- hdrezka_[2.2].user.js
+++ hdrezka_[2.2].user.js
@@ -8,7 +8,6 @@
 // @description:en  Disables all ads in player on site HDRezka.
 // @author          ANT0x1
 // @match           http://*.cdnapponline.com/*/html5?*
-// @match           http://*.staticnlcdn.com/*
 // @updateURL       https://openuserjs.org/meta/ANT0x1/HDRezka_Player_Ads_Disabler_[22].meta.js
 // @downloadURL     https://openuserjs.org/install/ANT0x1/HDRezka_Player_Ads_Disabler_[22].user.js
 // @icon            http://hdrezka.me/templates/hdrezka/images/favicon.ico
